01ec9d5be6e656ed55df2c437ce7991e21137250
José Carlos de Camargo <jose.camargo@MacBook-Pro-de-Jose.local>
2023-11-25 22:41:29 -0300

    feat: pagina busca de produtos

---
+++ src/app/(store)/search/page.tsx
@@ -0,0 +1,98 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Search() {
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultado para: <span className="font-semibold">moleton</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Link
+          href={`/product/moleton-never-stop-learning`}
+          className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+        >
+          <Image
+            src="/moletom-never-stop-learning.png"
+            width={480}
+            height={480}
+            quality={100}
+            alt=""
+            className="group-hover:scale-105 transition-transform duration-500"
+          />
+
+          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+            <span className="text-sm truncate">
+              Moleton Never Stop Learning
+            </span>
+            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+              {Number(129).toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+                maximumFractionDigits: 0,
+                minimumFractionDigits: 0,
+              })}
+            </span>
+          </div>
+        </Link>
+
+        <Link
+          href={`/product/moleton-never-stop-learning`}
+          className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+        >
+          <Image
+            src="/moletom-never-stop-learning.png"
+            width={480}
+            height={480}
+            quality={100}
+            alt=""
+            className="group-hover:scale-105 transition-transform duration-500"
+          />
+
+          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+            <span className="text-sm truncate">
+              Moleton Never Stop Learning
+            </span>
+            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+              {Number(129).toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+                maximumFractionDigits: 0,
+                minimumFractionDigits: 0,
+              })}
+            </span>
+          </div>
+        </Link>
+
+        <Link
+          href={`/product/moleton-never-stop-learning`}
+          className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+        >
+          <Image
+            src="/moletom-never-stop-learning.png"
+            width={480}
+            height={480}
+            quality={100}
+            alt=""
+            className="group-hover:scale-105 transition-transform duration-500"
+          />
+
+          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+            <span className="text-sm truncate">
+              Moleton Never Stop Learning
+            </span>
+            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+              {Number(129).toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+                maximumFractionDigits: 0,
+                minimumFractionDigits: 0,
+              })}
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  )
+}

b058bb581bbd903de87c289da08c7707518f33cf
José Carlos de Camargo <jccamargo15@gmail.com>
2024-05-10 17:55:54 -0300

    chore: remove ci test comments

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -105,12 +105,12 @@ export default async function ProductPage({ params }: ProductProps) {
             >
               G
             </button>
-            {/* <button
+            <button
               type="button"
               className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
             >
               GG
-            </button> */}
+            </button>
           </div>
         </div>
 
